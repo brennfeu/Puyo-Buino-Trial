5b107a46024fda41393e5d40b9f6e19ee39ba62e
Brenn <brenn.constant@gmail.com>
2019-05-16 16:58:27 +0200

    stuff

--- project.ino
+++ project.ino
@@ -1,12 +1,148 @@
 #include <Gamebuino-Meta.h>
 #include <time.h>
 
+//RED_STAND
+const uint16_t RED_STAND_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0xe8e4, 0xe8e4, 0xe8e4, 0xa254,
+  0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4,
+  0xe8e4, 0x0000, 0x8802, 0x0000, 0xe8e4,
+  0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4,
+  0xa254, 0xe8e4, 0xe8e4, 0xe8e4, 0xa254,
+};
+Image RED_STAND(RED_STAND_DATA);
+
+
+//BLUE_STAND
+const uint16_t BLUE_STAND_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0x051d, 0x051d, 0x051d, 0xa254,
+    0x051d, 0xffff, 0x051d, 0xffff, 0x051d,
+    0x051d, 0x0000, 0x051d, 0x0000, 0x051d,
+    0x051d, 0x051d, 0x051d, 0x051d, 0x051d,
+    0xa254, 0x051d, 0x051d, 0x051d, 0xa254,
+};
+Image BLUE_STAND(BLUE_STAND_DATA);
+
+
+//GREEN_STAND
+const uint16_t GREEN_STAND_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0x2589, 0x2589, 0x2589, 0xa254,
+    0xb723, 0xffff, 0x2589, 0xffff, 0xb723,
+    0xffff, 0xb723, 0x2589, 0xb723, 0xffff,
+    0x2589, 0x2589, 0x2589, 0x2589, 0x2589,
+    0xa254, 0x2589, 0x2589, 0x2589, 0xa254,
+};
+Image GREEN_STAND(GREEN_STAND_DATA);
+
+
+//YELLOW_STAND
+const uint16_t YELLOW_STAND_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0xff80, 0xff80, 0xff80, 0xa254,
+    0xff80, 0xff80, 0xff80, 0xff80, 0xff80,
+    0xfbe4, 0xfbe4, 0xff80, 0xfbe4, 0xfbe4,
+    0xff80, 0xff80, 0xff80, 0xff80, 0xff80,
+    0xa254, 0xff80, 0xff80, 0xff80, 0xa254,
+};
+Image YELLOW_STAND(YELLOW_STAND_DATA);
+
+
+//RED_BLINK
+const uint16_t RED_BLINK_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0xe8e4, 0xe8e4, 0xe8e4, 0xa254,
+    0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4,
+    0xe8e4, 0x8802, 0x8802, 0x8802, 0xe8e4,
+    0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4, 0xe8e4,
+    0xa254, 0xe8e4, 0xe8e4, 0xe8e4, 0xa254,
+};
+Image RED_BLINK(RED_BLINK_DATA);
+
+
+//BLUE_BLINK
+const uint16_t BLUE_BLINK_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0x051d, 0x051d, 0x051d, 0xa254,
+    0x051d, 0x0000, 0x051d, 0x0000, 0x051d,
+    0x051d, 0xffff, 0x051d, 0xffff, 0x051d,
+    0x051d, 0x051d, 0x051d, 0x051d, 0x051d,
+    0xa254, 0x051d, 0x051d, 0x051d, 0xa254,
+};
+Image BLUE_BLINK(BLUE_BLINK_DATA);
+
+
+//GREEN_BLINK
+const uint16_t GREEN_BLINK_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0x2589, 0x2589, 0x2589, 0xa254,
+    0x2589, 0x2589, 0x2589, 0x2589, 0x2589,
+    0xb723, 0xb723, 0x2589, 0xb723, 0xb723,
+    0x2589, 0x2589, 0x2589, 0x2589, 0x2589,
+    0xa254, 0x2589, 0x2589, 0x2589, 0xa254,
+};
+Image GREEN_BLINK(GREEN_BLINK_DATA);
+
+
+//YELLOW_BLINK
+const uint16_t YELLOW_BLINK_DATA[] = {
+  5, 5, // largeur, hauteur
+  1, // images
+  0, // boucle image
+  0xa254, // couleur transparente
+  0, // mode couleur
+
+  0xa254, 0xff80, 0xff80, 0xff80, 0xa254,
+  0xffff, 0xffff, 0xff80, 0xffff, 0xffff,
+  0xffff, 0x0000, 0xff80, 0x0000, 0xffff,
+  0xffff, 0xffff, 0xff80, 0xffff, 0xffff,
+  0xa254, 0xff80, 0xff80, 0xff80, 0xa254,
+};
+Image YELLOW_BLINK(YELLOW_BLINK_DATA);
+
 int i = 0;
 int j = 0;
 
 int timerFrame = 0;
 int timerSecond = 0;
 bool gameOver = false;
+int state = 0;
 char colorList[4] = {'r', 'g', 'b', 'y'};
 
 char puyo01 = 'w';
@@ -21,19 +157,24 @@ int currentPuyoY = 0;
 int currentPuyoOrientation = 0;
 
 char board[12][6] = {
-    {'e', 'e', 'x', 'x', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'},
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'}, 
-    {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'x', 'x', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
+  {'e', 'e', 'e', 'e', 'e', 'e'},
 };
+int boardFall[12][6];
+
+String string1 = "";
+String string2 = "";
+String string3 = "";
 
 // SETUP CODE = ONCE
 void setup() {
@@ -47,21 +188,25 @@ void setup() {
 // INFINITE LOOP
 void loop() {
   // put your main code here, to run repeatedly:
-  while(!gb.update());
+  while (!gb.update());
   gb.display.clear();
 
-  if (!gameOver) {
-    // TIMER
+  if (!gameOver) {    
+    // GAME
     incrementTimer();
-    currentPuyoMove();
-    
+    if (state == 4) {spawnNew();}
+    if (state == 0) {currentPuyoMove();}
+    if (state == 0) {collisionCurrentPuyo();}
+    if (state == 1) {checkBoard();}
+    if (state == 2) {checkCombos();}
+
     // UI
-    drawUI();
     drawBoard();
     drawNextPuyos();
-  
+    drawUI();
+
     // TESTS
-    drawText("POP", "10", "PUYOS");
+    drawText("", state, "");
   }
   else {
     gb.display.setCursor(10, 10);
@@ -76,28 +221,40 @@ void loop() {
 // SYSTEM
 void resetGame() {
   // variables reset
-  timerSecond = 5;
+  timerSecond = 60;
   timerFrame = 0;
   gameOver = false;
-  currentPuyoX = 10;
-  currentPuyoY = 0;
+  state = 4;
 
   // board
-  for (i = 0; i<12; i++) {
-    for (j = 0; j<6; j++) {
+  for (i = 0; i < 12; i++) {
+    for (j = 0; j < 6; j++) {
       board[i][j] = 'e';
+      boardFall[i][j] = 0;
     }
   }
   board[0][2] = 'x';
   board[0][3] = 'x';
 
   // puyos
-  puyo01 = getRandomColor();
-  puyo02 = getRandomColor();
   puyo11 = getRandomColor();
   puyo12 = getRandomColor();
   puyo21 = getRandomColor();
   puyo22 = getRandomColor();
+}
+void spawnNew() {
+    currentPuyoOrientation = 0;
+    currentPuyoX = 10;
+    currentPuyoY = 0;
+  
+    puyo01 = puyo11;
+    puyo02 = puyo12;
+    puyo11 = puyo21;
+    puyo12 = puyo22;
+    puyo21 = getRandomColor();
+    puyo22 = getRandomColor();
+
+    state = 0;
 }
 void setDisplayColor(char _color) {
   if (_color == 'b') {
@@ -136,48 +293,169 @@ void currentPuyoMove() {
   if (gb.buttons.repeat(BUTTON_DOWN, 0)) {
     currentPuyoY += 4;
   }
-  else if(timerFrame%10 == 0) {
+  else if (timerFrame % 10 == 0) {
     currentPuyoY++;
   }
+
+  bool pressR = false;
+  bool pressL = false;
   if (gb.buttons.repeat(BUTTON_RIGHT, 5) && currentPuyoX < 25) {
     currentPuyoX += 5;
+    pressR = true;
   }
   if (gb.buttons.repeat(BUTTON_LEFT, 5) && currentPuyoX > 0) {
     currentPuyoX -= 5;
+    pressL = true;
   }
-  
+
+  bool pressB = false;
+  bool pressA = false;
   if (gb.buttons.pressed(BUTTON_B)) {
     currentPuyoOrientation++;
+    pressB = true;
   }
   if (gb.buttons.pressed(BUTTON_A)) {
     currentPuyoOrientation--;
+    pressA = true;;
   }
-  
-  if(currentPuyoOrientation >= 4) {
+
+  // boucle
+  if (currentPuyoOrientation >= 4) {
     currentPuyoOrientation -= 4;
   }
   if (currentPuyoOrientation <= -1) {
     currentPuyoOrientation += 4;
   }
+  // collisions variables
+  int currentYTile = ceil(currentPuyoY / 5);
+  int currentXTile = currentPuyoX / 5;
+  int current2YTile = ceil(currentPuyoY / 5);
+  int current2XTile = currentPuyoX / 5;
+  if (currentPuyoOrientation == 0) {
+    current2YTile -= 1;
+  }
+  if (currentPuyoOrientation == 1) {
+    current2XTile += 1;
+  }
+  if (currentPuyoOrientation == 2) {
+    current2YTile += 1;
+  }
+  if (currentPuyoOrientation == 3) {
+    current2XTile -= 1;
+  }
 
-  if (currentPuyoOrientation == 3 && currentPuyoX <= 0) {
-    currentPuyoX += 5;
+  if (currentPuyoOrientation == 1 && hasCollision(current2XTile, current2YTile)) {
+    if (!hasCollision(currentXTile - 1, currentYTile)) {
+      currentPuyoX -= 5;
+    }
+    else if (pressB) {
+      currentPuyoOrientation++;
+    }
+    else if (pressA) {
+      currentPuyoOrientation--;
+    }
   }
-  if (currentPuyoOrientation == 1 && currentPuyoX >= 25) {
-    currentPuyoX -= 5;
+  else if (currentPuyoOrientation == 3 && hasCollision(current2XTile, current2YTile)) {
+    if (!hasCollision(currentXTile + 1, currentYTile)) {
+      currentPuyoX += 5;
+    }
+    else if (pressB) {
+      currentPuyoOrientation++;
+    }
+    else if (pressA) {
+      currentPuyoOrientation--;
+    }
   }
+  else if (hasCollision(current2XTile, current2YTile) || hasCollision(currentXTile, currentYTile)) {
+    if (pressR) {
+      currentPuyoX -= 5;
+    }
+    else if (pressL) {
+      currentPuyoX += 5;
+    }
+  }
+}
+void collisionCurrentPuyo() {
+  int currentYTile = ceil((currentPuyoY + 4) / 5);
+  int currentXTile = currentPuyoX / 5;
+
+  if ( hasCollision(currentXTile, currentYTile) || (currentPuyoOrientation == 1 && hasCollision(currentXTile + 1, currentYTile)) ||
+       (currentPuyoOrientation == 3 && hasCollision(currentXTile - 1, currentYTile)) || (currentPuyoOrientation == 2 && hasCollision(currentXTile, currentYTile + 1))) {
+    currentPuyoY -= 5;
+    currentYTile = ceil((currentPuyoY + 4) / 5);
+
+    if (currentPuyoOrientation == 0) {
+      board[currentYTile][currentXTile] = puyo01;
+      board[currentYTile - 1][currentXTile] = puyo02;
+    }
+    else if (currentPuyoOrientation == 1) {
+      board[currentYTile][currentXTile] = puyo01;
+      board[currentYTile][currentXTile + 1] = puyo02;
+    }
+    else if (currentPuyoOrientation == 2) {
+      board[currentYTile][currentXTile] = puyo01;
+      board[currentYTile + 1][currentXTile] = puyo02;
+    }
+    else {
+      board[currentYTile][currentXTile] = puyo01;
+      board[currentYTile][currentXTile - 1] = puyo02;
+    }
+
+    state = 1;
+  }
+}
+bool hasCollision(int _x, int _y) {
+  if (_y >= 12 || _y <= -1 || _x >= 6 || _x <= -1) {
+    return true;
+  }
+  if (board[_y][_x] != 'e' && board[_y][_x] != 'x') {
+    return true;
+  }
+  return false;
+}
+void checkBoard() {
+  bool chute = false;
+  for (i = 0; i < 11; i++) {
+    for (j = 0; j < 6; j++) {
+      if (board[i+1][j] == 'e' && board[i][j] != 'x' && board[i][j] != 'e') {
+        chute = true;
+        boardFall[i][j] += 1;
+        if (boardFall[i][j] >= 5) {
+          boardFall[i+1][j] = boardFall[i][j] - 5;
+          boardFall[i][j] = 0;
+          board[i+1][j] = board[i][j];
+          board[i][j] = 'e';
+        }
+      }
+      else {
+        boardFall[i][j] = 0;
+      }
+    }
+  }
+  for (j = 0; j < 6; j++) {
+    boardFall[11][j] = 0;
+  }
+  if (!chute) {
+    state = 2;
+  }
+}
+void checkCombos() {
+  state = 4;
 }
 
 // DRAW
 void drawUI() {
   setDisplayColor('w');
-  
+
   // timer
   gb.display.setCursor(51, 51);
   gb.display.printf("%is", timerSecond);
-  
+
   // main board
   gb.display.drawRect(1, 1, 32, 62);
+  gb.display.setColor(BLACK);
+  gb.display.drawLine(0, 0, 32, 0);
+  gb.display.setColor(WHITE);
 
   // next puyos
   gb.display.drawRect(35, 3, 7, 12);
@@ -190,16 +468,16 @@ void drawUI() {
   gb.display.print("Buino");
 }
 void drawBoard() {
-  for (i = 0; i<12; i++) {
-    for (j = 0; j<6; j++) {
+  for (i = 0; i < 12; i++) {
+    for (j = 0; j < 6; j++) {
       if (board[i][j] == 'e') {
-        // drawPuyo(j*5+2, i*5+2, 'p');
+        // empty
       }
       else if (board[i][j] == 'x') {
-        drawCross(j*5+2, i*5+2);
+        drawCross(j * 5 + 2, i * 5 + 2);
       }
       else {
-        drawPuyo(j*5+2, i*5+2, board[i][j]);
+        drawPuyo(j * 5 + 2, i * 5 + 2 + boardFall[i][j], board[i][j]);
       }
     }
   }
@@ -209,21 +487,23 @@ void drawNextPuyos() {
   drawPuyo(36, 9, puyo12);
   drawPuyo(38, 18, puyo21);
   drawPuyo(38, 23, puyo22);
-  drawPuyo(currentPuyoX+2, currentPuyoY+2, puyo01);
-  if (currentPuyoOrientation == 0) {
-    drawPuyo(currentPuyoX+2, currentPuyoY-3, puyo02);
-  }
-  else if (currentPuyoOrientation == 1) {
-    drawPuyo(currentPuyoX+7, currentPuyoY+2, puyo02);
-  }
-  else if (currentPuyoOrientation == 2) {
-    drawPuyo(currentPuyoX+2, currentPuyoY+7, puyo02);
-  }
-  else if (currentPuyoOrientation == 3) {
-    drawPuyo(currentPuyoX-3, currentPuyoY+2, puyo02);
+  if (state == 0) {
+    drawPuyo(currentPuyoX + 2, currentPuyoY + 2, puyo01);
+    if (currentPuyoOrientation == 0) {
+      drawPuyo(currentPuyoX + 2, currentPuyoY - 3, puyo02);
+    }
+    else if (currentPuyoOrientation == 1) {
+      drawPuyo(currentPuyoX + 7, currentPuyoY + 2, puyo02);
+    }
+    else if (currentPuyoOrientation == 2) {
+      drawPuyo(currentPuyoX + 2, currentPuyoY + 7, puyo02);
+    }
+    else if (currentPuyoOrientation == 3) {
+      drawPuyo(currentPuyoX - 3, currentPuyoY + 2, puyo02);
+    }
   }
 }
-void drawText(String _string1, String _string2, String _string3) {
+void drawText(String _string1, int _string2, String _string3) {
   setDisplayColor('w');
   gb.display.setCursor(56, 26);
   gb.display.print(_string1);
@@ -233,12 +513,41 @@ void drawText(String _string1, String _string2, String _string3) {
   gb.display.print(_string3);
 }
 void drawPuyo(int _x, int _y, char _color) {
-  setDisplayColor(_color);
-  gb.display.fillRect(_x+1, _y, 3, 5);
-  gb.display.fillRect(_x, _y+1, 5, 3);
+  if (_color == 'r') {
+    if (timerFrame >= 25 && timerSecond%4 == 0) {
+      gb.display.drawImage(_x, _y, RED_BLINK);
+    }
+    else {
+      gb.display.drawImage(_x, _y, RED_STAND);
+    }
+  }
+  if (_color == 'b') {
+    if (timerFrame >= 25 && timerSecond%4 == 1) {
+      gb.display.drawImage(_x, _y, BLUE_STAND);
+    }
+    else {
+      gb.display.drawImage(_x, _y, BLUE_STAND);
+    }
+  }
+  if (_color == 'g') {
+    if (timerFrame >= 25 && timerSecond%4 == 2) {
+      gb.display.drawImage(_x, _y, GREEN_BLINK);
+    }
+    else {
+      gb.display.drawImage(_x, _y, GREEN_STAND);
+    }
+  }
+  if (_color == 'y') {
+    if (timerFrame >= 25 && timerSecond%4 == 3) {
+      gb.display.drawImage(_x, _y, YELLOW_BLINK);
+    }
+    else {
+      gb.display.drawImage(_x, _y, YELLOW_STAND);
+    }
+  }
 }
 void drawCross(int _x, int _y) {
   setDisplayColor('r');
-  gb.display.drawLine(_x, _y, _x+4, _y+4);
-  gb.display.drawLine(_x+4, _y, _x, _y+4);
+  gb.display.drawLine(_x, _y, _x + 4, _y + 4);
+  gb.display.drawLine(_x + 4, _y, _x, _y + 4);
 }
